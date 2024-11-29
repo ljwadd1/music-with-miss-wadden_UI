3f0334c0dac6cdd344624be2a041517b8ca25eb1
ljwadd1 <144718706+ljwadd1@users.noreply.github.com>
2024-11-29 16:40:17 -0400

    Setup navigation structure with child components renderring within parent "App" component using Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <div>
         <h1>Welcome!</h1>
+        <p>This is the parent page, "App"</p>
+      </div>
+
+      <div>
+        <p>Below is where the child pages will render</p>
+        <Outlet />
       </div>
     </>
   )
